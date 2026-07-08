c9738cc91f847d692589843b3f38ac54b2f0a2d0
jeffer91 <jeffersonvillarreal91@gmail.com>
2026-07-08 07:54:11 -0500

    bloque-03-carga-lectura-archivos

--- electron/rc-preload.js
+++ electron/rc-preload.js
@@ -3,5 +3,7 @@ const { contextBridge, ipcRenderer } = require('electron');
 contextBridge.exposeInMainWorld('rcApi', {
   getAppInfo: () => ipcRenderer.invoke('app:get-info'),
   healthCheck: () => ipcRenderer.invoke('app:health-check'),
-  selectFiles: (options) => ipcRenderer.invoke('dialog:select-files', options)
+  selectFiles: (options) => ipcRenderer.invoke('dialog:select-files', options),
+  importDocument: (payload) => ipcRenderer.invoke('files:import-document', payload),
+  getImportedFiles: () => ipcRenderer.invoke('files:get-imported')
 });
