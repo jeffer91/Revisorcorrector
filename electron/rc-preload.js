f48d793ffef9b59edb501db50c5f8e754d02104b
jeffer91 <jeffersonvillarreal91@gmail.com>
2026-07-08 08:17:12 -0500

    bloque-09-pdf-real-apertura-reportes

--- electron/rc-preload.js
+++ electron/rc-preload.js
@@ -6,6 +6,7 @@ contextBridge.exposeInMainWorld('rcApi', {
   selectFiles: (options) => ipcRenderer.invoke('dialog:select-files', options),
   importDocument: (payload) => ipcRenderer.invoke('files:import-document', payload),
   getImportedFiles: () => ipcRenderer.invoke('files:get-imported'),
+  openPath: (filePath) => ipcRenderer.invoke('files:open-path', filePath),
   analyzePeaAlignment: () => ipcRenderer.invoke('analysis:pea-alignment'),
   runInstitutionalReview: () => ipcRenderer.invoke('analysis:institutional-review')
 });
