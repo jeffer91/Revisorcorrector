833ac78a21241d7e98ddb078c3ff694861efae53
jeffer91 <jeffersonvillarreal91@gmail.com>
2026-07-24 00:35:59 -0500

    exponer lectura y exportacion de Firestore

--- preload.js
+++ preload.js
@@ -4,6 +4,9 @@ contextBridge.exposeInMainWorld('migrador', {
   seleccionarExcel: () => ipcRenderer.invoke('excel:seleccionar'),
   analizar: () => ipcRenderer.invoke('excel:analizar'),
   migrar: () => ipcRenderer.invoke('firebase:migrar'),
+  obtenerResumenFirebase: () => ipcRenderer.invoke('firebase:resumen'),
+  leerColeccionFirebase: (collectionName) => ipcRenderer.invoke('firebase:leer-coleccion', collectionName),
+  exportarFirebase: (collectionNames) => ipcRenderer.invoke('firebase:exportar', collectionNames),
   onProgreso: (callback) => {
     const listener = (_event, data) => callback(data);
     ipcRenderer.on('migracion:progreso', listener);
